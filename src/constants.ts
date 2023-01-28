7a08edafc968c5188f24e24c3e35a033cabde1ae
ozne <florenzodinglasan@gmail.com>
2023-01-29 06:59:03 +0800

    added constants

---
+++ src/constants.ts
@@ -0,0 +1,31 @@
+export enum Direction {
+  NORTH = 'NORTH',
+  SOUTH = 'SOUTH',
+  EAST = 'EAST',
+  WEST = 'WEST',
+  NULL = 'NULL',
+}
+
+export interface Position {
+  x: number;
+  y: number;
+}
+
+export interface Robot {
+  placed: boolean;
+  position: Position;
+  direction: Direction;
+}
+
+export const TABLETOP_SIZE: number = 5;
+
+export enum StringCommands {
+  LEFT = 'LEFT',
+  RIGHT = 'RIGHT',
+  REPORT = 'REPORT',
+  MOVE = 'MOVE',
+}
+
+export interface placeCommand {
+  newCommand: (x: number, y: number, direction: Direction) => Robot;
+}
